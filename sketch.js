e320b8a931c8a0001689add59ea85132419e3231
aArmy310 <74861157+aArmy310@users.noreply.github.com>
2020-12-11 15:37:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+function setup() {
+  createCanvas(1600,400);
+  var car,wall;
+  car=createSprite(50, 200, 50, 50);
+  wall=createSprite(1500,200,60,height/2)
+  speed=randonm(55,90)
+  weight=random(400,1500)
+  car.velocityX = speed;
+}
+var speed,weight;
+
+function draw() {
+  background(255,255,255);
+
+  if(wall.x-car.x <(car.width)/2){
+    car.velocityX=0;
+    var deformation =0.5*weight*speed*speed/22500;
+    if(deformation>180) {
+      car.shapeColor = color(255,0,0);
+    }
+    if(deformation<180&& deformation>100){
+      car.shapeColor=color(230,230,0);
+    }
+    if (deformation<100)
+
+    {
+      car.shapeColor=color(0,255,0);
+    }
+  }
+  wall.shapeColor (80,80,80);
+  drawSprites();
+}
